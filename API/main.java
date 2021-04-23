8cea9e4e84005f03d1309dfdad01b28c88ad0041
2041813809 <71110483+2041813809@users.noreply.github.com>
2021-04-23 09:54:02 +0800

    Add files via upload

---
+++ API/main.java
@@ -0,0 +1,13 @@
+package API;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner ac = new Scanner(System.in);
+        System.out.println("请输入数据：");
+        String line = ac.nextLine();
+
+        System.out.println("你输入的数据是："+line);
+    }
+}
